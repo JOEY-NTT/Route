ae04834b14590d6aea507ce86e7efecd1a28a866
JOEY-NTT <lizhenyang96@gmail.com>
2025-12-19 11:59:29 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Route/', // 確保對應您的 Repo 名稱
+    base: '/Route/', // <--- 這行最重要，否則發佈後會是白畫面
     plugins: [react()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
