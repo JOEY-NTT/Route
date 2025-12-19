f1d6abafa397d445cf8c8bbbd0c896a6765129d4
JOEY-NTT <lizhenyang96@gmail.com>
2025-12-19 14:13:01 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Route/', // 確保對應 GitHub 儲存庫名稱
+    base: '/Route/', 
     plugins: [react()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
